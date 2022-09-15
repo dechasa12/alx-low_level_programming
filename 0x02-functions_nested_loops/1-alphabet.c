6dae788764686c241e48044d050794d4876082d1
dechasa <dechasashimels95@gmail.com>
2022-09-15 01:46:03 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
-
-
 /**
-
  * main - check the code
-
  *
-
  * Return: Always 0.
-
  */
 
-int main(void)
-
+void print_alphabet(void)
 {
+	char alp = 'a';
 
-    print_alphabet();
-
-    return (0);
-
+	while (alp <= z)
+	{
+		_putchar (alp);
+		alp++;
+	}
+	_putchar('\n');
 }

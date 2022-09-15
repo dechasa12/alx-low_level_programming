4e21dbcadec7b6c574b2503f833582f5a8348501
dechasa <dechasashimels95@gmail.com>
2022-09-15 08:06:35 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/***
+/**
 * jack_bauer - prints every minute of the day of jack bauer
 *
 *Return: Always 0.
@@ -18,7 +18,7 @@ void jack_bauer(void)
 			_putchar((hr / 10) + '0');
 			_putchar((hr % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + '0');	
+			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}

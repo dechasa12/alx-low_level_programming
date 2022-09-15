371bd20cc30fab9bf8e70305177f8ce3762a3408
dechasa <dechasashimels95@gmail.com>
2022-09-15 03:04:58 -0700

    TASK 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _abs - returns abs value of int
+*
+* @n: integer n
+*
+* Return: Void.
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return(n);
+}

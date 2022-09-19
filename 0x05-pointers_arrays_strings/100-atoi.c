c1dfbb8f30d467bc0160387b6342768e504a938a
dechasa <dechasashimels95@gmail.com>
2022-09-19 12:12:30 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,9 +19,9 @@ int _atoi(char *s)
 
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
-												else if (num > 0)
+		
+		else if (num > 0)
 			break;
-											} while (*s++);
-	
+	} while (*s++)
 	return (num * sign);
 }

927f58555f38b3ed8ffb848f77a67b9f08e00687
dechasa <dechasashimels95@gmail.com>
2022-09-22 07:41:12 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,27 +9,23 @@ nclude "main.h"
 
 char *_strcat(char *dest, char *src)
 {
-		int i;
+	int i;
 
-			int j;
+	int j;
 
-				i = 0;
+	i = 0;
 
-					j = 0;
+	j = 0;
 
-						while (dest[i] != '\0')
-								{
-											i++;
-												}
+	while (dest[i] != '\0')								{
+												i++;									}
 
-							while (src[j] != '\0')
-									{
-												dest[i] = src[j];
-
-														j++;
-
-																i++;
-																	}
-								dest[i] = '\0';
-									return (dest);
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+												j++;
+												i++;
+											}			
+	dest[i] = '\0';
+	return (dest);
 }

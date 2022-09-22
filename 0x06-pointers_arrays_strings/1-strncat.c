82c3528d3ed66aa4507a9948cff1047436ac36f6
dechasa <dechasashimels95@gmail.com>
2022-09-22 07:43:26 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * * _strncat - two words
- * * @dest : pointer to char param
- * * @src : pointer to char param
- * * @n : int parameter
- * * Return: *dest
- * */
+* _strncat - two words
+* @dest : pointer to char param
+* @src : pointer to char param
+* @n : int parameter
+* Return: *dest
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {

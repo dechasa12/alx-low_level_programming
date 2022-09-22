745e16152c7c1c3fefe660cefdd28bbbbc58cc1b
dechasa <dechasashimels95@gmail.com>
2022-09-22 07:44:30 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * * _strncpy - two words
+ * * @dest : pointer to char params
+ * * @src : pointer to char params
+ * * @n : int params
+ * * Return: *dest
+ * */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+		int a = 0, b = 0;
+
+			while (src[b])
+					{
+								b++;
+									}
+
+				while (a < n && src[a])
+						{
+									dest[a] = src[a];
+											a++;
+												}
+
+					while (a < n)
+							{	
+										dest[a] = '\0';
+												a++;
+													}
+						return (dest);
+}

344ddc58108c5f5acc2accce7af2a659355afc48
dechasa <dechasashimels95@gmail.com>
2022-09-22 08:10:35 -0700

    dechasa

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,62 @@
-nclude "main.h"
+#include "main.h"
 
 /**
-*_strcat - function commute srtings
-* @dest: param pointer to a char
-* @src: param pointer to a char
-* Return: return value of dest
-*/
+ *
+ *  * *_strcat - function commute srtings
+ *
+ *   * @dest: param pointer to a char
+ *
+ *    * @src: param pointer to a char
+ *
+ *     * Return: return value of dest
+ *
+ *      */
+
+
 
 char *_strcat(char *dest, char *src)
+
 {
-	int i;
 
-	int j;
+		int i;
 
-	i = 0;
+			int j;
 
-	j = 0;
 
-	while (dest[i] != '\0')								{
-												i++;									}
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-												j++;
-												i++;
-											}			
-	dest[i] = '\0';
-	return (dest);
+				i = 0;
+
+					j = 0;
+
+
+
+						while (dest[i] != '\0')
+
+								{
+
+											i++;
+
+												}
+
+
+
+							while (src[j] != '\0')
+
+									{
+
+												dest[i] = src[j];
+
+														j++;
+
+																i++;
+
+																	}
+
+
+
+								dest[i] = '\0';
+
+									return (dest);
+
 }
+

a72d9faf32796cf1f403e54094b5d8dbdc210175
dechasa <dechasashimels95@gmail.com>
2022-09-23 10:59:03 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * * _strncpy - two words
- * * @dest : pointer to char params
- * * @src : pointer to char params
- * * @n : int params
- * * Return: *dest
- * */
+* _strncpy - two words
+* @dest : pointer to char params
+* @src : pointer to char params
+* @n : int params
+* Return: *dest
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {

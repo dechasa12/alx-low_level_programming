8b15c727dc6ad5dbded5322ff672c9fd8ac5d313
dechasa <dechasashimels95@gmail.com>
2022-09-23 10:37:58 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,11 +11,13 @@ void reverse_array(int *a, int n)
 	int tmp, index;
 
 	for (index = n - 1; index >= n / 2; index--)
+
 	{
 		tmp = a[n - 1 - index];
 
 		a[n - 1 - index] = a[index];
 		
-		a[index] = tmp;											
+		a[index] = tmp;
+
 	}
 }

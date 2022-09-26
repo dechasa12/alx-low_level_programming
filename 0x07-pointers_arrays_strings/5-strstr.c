21d6583e35833a3599bc9781e51d8a9389d6dc3a
dechasa <dechasashimels95@gmail.com>
2022-09-26 07:53:05 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* *_strstr - locates a substring
+* _strstr - locates a substring
 * @haystack: the longer string to search
 * @needle: the substring to search for
 * Return: a pointer to the beginning of the located substring, or NULL if
@@ -12,7 +12,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0, j = 0;
-	
+
 	while (haystack[i])
 	{
 		while (needle[j] && (haystack[i] == needle[0]))

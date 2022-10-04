4d86b7339526a2a0cadea50c56622b309dbb9704
dechasa <dechasashimels95@gmail.com>
2022-10-04 11:53:58 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -77,7 +77,7 @@ char **strtow(char *str)
 		while (str[index] == ' ')
 			index++;
 
-		letters = word_len(str + index)
+		letters = word_len(str + index);
 
 		strings[w] = malloc(sizeof(char) * (letters + 1));
 

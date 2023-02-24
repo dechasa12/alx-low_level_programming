c1af888d4430b6ca557222525f14d923218b8f99
Dechasa Shimels <59132317+dechasa12@users.noreply.github.com>
2023-02-24 09:19:37 -0800

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-module for perimeter of island
+module for perimeter of islands
 """
 
 
@@ -25,4 +25,3 @@ def island_perimeter(grid):
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
     return size * 4 - edges * 2
-  
